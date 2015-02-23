416aff11a3eee59e043774844e5ed5367e4dcdad
kyokomi <kyoko1220adword@gmail.com>
2015-02-23 09:56:41 +0900

    DDL: time型の消し忘れ

--- test/mysql/ddl/create_table_date.sql
+++ test/mysql/ddl/create_table_date.sql
@@ -2,7 +2,6 @@
 CREATE TABLE goma_date_types (
   id                  BIGINT PRIMARY KEY,
   date_columns        DATE,
-  time_columns        TIME,
   datetime_columns    DATETIME,
   timestamp_columns   TIMESTAMP
 );
