a8adc972f506e261b4f89c9ced4e7de89b893629
kyokomi <kyoko1220adword@gmail.com>
2015-06-25 08:04:21 +0900

    added local_test

--- local_test.sh
+++ local_test.sh
@@ -4,6 +4,8 @@ go generate github.com/kyokomi/goma/gen/goma
 go install github.com/kyokomi/goma/gen/goma
 go generate github.com/kyokomi/goma/test/mysql
 go generate github.com/kyokomi/goma/test/postgres
+go test .
+go test ./gen/goma/...
 go test ./test/...
 
 golint ./test/... | grep -v ego
